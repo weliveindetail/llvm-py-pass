997ecc77f7ec5b7ac21ddad279b71471e3b6d90f
Stefan Gränitz <stefan.graenitz@gmail.com>
2024-10-21 12:20:33 +0200

    Load script from pass.py
    ```
    > cat test.c
    int main() { return 0; }
    
    > cat pass.py
    print("Hello world")
    
    > clang -fpass-plugin=$(pwd)/libllvm-py-pass.dylib -O1 test.c
    Obtaining pass plugin info
    Adding pass to optimizer pipeline
    Running pass on function: main
    Running out-of-tree pass: /path/to/llvm-py-pass/build/pass.py
      python version: 3.10.12 (main, Jun 20 2023, 17:00:24) [Clang 14.0.3 (clang-1403.0.22.14.1)]
      llvmlite version: 0.43.0-dirty
    Hello world
    ```

--- py.cpp
+++ py.cpp
@@ -1,20 +1,45 @@
 #include <pybind11/embed.h>
 
 #include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <unistd.h>
+#include <limits.h>
 
 namespace py = pybind11;
 
 void run() {
+  char cwd[PATH_MAX];
+  if (!getcwd(cwd, sizeof(cwd))) {
+    fprintf(stderr, "Failed to run out-of-tree pass: Cannot obtain current working directory\n");
+    return;
+  }
+
+  auto pass_script = std::string(cwd) + "/pass.py";
+  std::ifstream file(pass_script);
+  if (!file) {
+    fprintf(stderr, "Failed to run out-of-tree pass: Cannot find script file %s\n", pass_script.c_str());
+    return;
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string script = buffer.str();
+
   try {
     py::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
-    py::print("Hello Python! version:");
+    fprintf(stderr, "Running out-of-tree pass: %s\n", pass_script.c_str());
     py::module_ sys = py::module_::import("sys");
-    py::print(sys.attr("version"));
-
-    py::print("Hello llvmlite! version:");
+    auto py_version = py::cast<std::string>(sys.attr("version"));
+    fprintf(stderr, "  python version: %s\n", py_version.c_str());
     py::module_ llvmlite = py::module_::import("llvmlite");
-    py::print(llvmlite.attr("__version__"));
+    auto llvmlite_version = py::cast<std::string>(llvmlite.attr("__version__"));
+    fprintf(stderr, "  llvmlite version: %s\n", llvmlite_version.c_str());
+
+    py::exec(script);
   } catch (...) {
     exit(1);
   }
